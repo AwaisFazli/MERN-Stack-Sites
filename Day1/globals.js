c69b9aa2c903c229a22b52bc5f6df9933aef984e
AwaisFazli <awaisfazli947@gmail.com>
2022-11-21 20:59:29 +0500

    Global Variables

--- Day1/globals.js
+++ Day1/globals.js
@@ -6,4 +6,12 @@
 // module // info about current module
 // process // info about env where the program is being executed
 
-console.log(process);
+// console.log(process);
+
+setInterval(() => {
+  console.log("Hehehe");
+}, 1000);
+
+setTimeout(() => {
+  console.log("3000 nano seconds passed hehehe");
+}, 3000);
