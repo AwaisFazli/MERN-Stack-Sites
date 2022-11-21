1af5c2b5ecf8896d987a906fb21f34801da6a1cd
AwaisFazli <awaisfazli947@gmail.com>
2022-11-21 20:40:04 +0500

    Day1

---
+++ Day1/globals.js
@@ -0,0 +1,9 @@
+// Main Global Variables
+
+// __dirname  // gives the current directory name
+// __filename // gives current file name
+// require // function to use modules (just like import)
+// module // info about current module
+// process // info about env where the program is being executed
+
+console.log(process);
