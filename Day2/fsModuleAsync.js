b83f7d9fb06fc550a183331026538fe841c017f8
AwaisFazli <awaisfazli947@gmail.com>
2022-11-23 22:04:47 +0500

    FS Module Async

--- Day2/fsModuleAsync.js
+++ Day2/fsModuleAsync.js
@@ -1,6 +1,36 @@
 const { readFile, writeFile } = require("fs");
 
-const first = readFile("./folder/first.txt");
-const second = readFile("./folder/second.txt", "utf8");
+readFile("./folder/first.txt", "utf-8", (error, result) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const first = result;
 
-console.log(first, second);
+  readFile("./folder/second.txt", "utf-8", (error, result) => {
+    if (error) {
+      console.log(error);
+    }
+    const second = result;
+
+    writeFile(
+      "./folder/resultAsync.txt",
+      `Heres The Result:,${first} ${second}`,
+      (error, result) => {
+        if (error) {
+          console.log(error);
+        }
+        console.log(result);
+      }
+    );
+  });
+});
+
+// readFile("./folder/second.txt", "utf-8", (error, result) => {
+//   if (error) {
+//     console.log(error);
+//   }
+//   second = result;
+// });
+
+// console.log(first);
