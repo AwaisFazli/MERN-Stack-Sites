fde9a5382098fb421aa7580a90aa2dbffdcb4245
AwaisFazli <awaisfazli947@gmail.com>
2022-11-28 22:24:07 +0500

    Streams

--- Day4/streams.js
+++ Day4/streams.js
@@ -1,6 +1,6 @@
 const { createReadStream } = require("fs");
 
-const stream = createReadStream("./data/largeData.txt");
+const stream = createReadStream("./data/largeData.txt", { encoding: "utf-8" });
 
 stream.on("data", (result) => {
   console.log(result);
