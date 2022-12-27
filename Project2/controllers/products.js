944e1c8be7506d8dc8211a91b8ce2ba37851a16a
AwaisFazli <awaisfazli947@gmail.com>
2022-12-27 22:08:53 +0500

    Finished

--- Project2/controllers/products.js
+++ Project2/controllers/products.js
@@ -11,7 +11,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
   let results = Product.find(queryObject);
@@ -35,7 +35,27 @@ const getAllProducts = async (req, res) => {
     const fieldslist = fields.replace(",", " ");
     results = results.select(fieldslist);
   }
-
+  if (numericFilters) {
+    const operatorMap = {
+      "<": "$lt",
+      "<=": "$lte",
+      "=": "$e1",
+      ">": "$gt",
+      ">=": "$gte",
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ["price", "ratings"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
   const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 10;
   const skip = (page - 1) * limit;
