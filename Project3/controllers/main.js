e532c9e6fbd50fd817ff803f7e7de5f8f0fa4456
AwaisFazli <awaisfazli947@gmail.com>
2022-12-30 17:30:45 +0500

    Finalized

--- Project3/controllers/main.js
+++ Project3/controllers/main.js
@@ -18,16 +18,27 @@ const login = async (req, res) => {
     { expiresIn: "30d" }
   );
 
-  console.log(username, password);
-
   res.status(200).json({ msg: "User Created", token });
 };
 
 const dashboard = async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100);
-  res
-    .status(200)
-    .json({ msg: `Hello`, secret: `your lucky number is ${luckyNumber}` });
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("bearer ")) {
+    throw new CustomAPIError("No Token Provided", 401);
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    res.status(200).json({
+      msg: `Hello ${decoded.username}`,
+      secret: `your lucky number is ${luckyNumber}`,
+    });
+  } catch (error) {
+    throw new CustomAPIError("Not Authorized To Access This Route", 401);
+  }
 };
 
 module.exports = { login, dashboard };
